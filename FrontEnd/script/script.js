a93ebf0b82acdc94ec4a4e0e4dc89870d3eaa4ee
Kevin-tre <kevintrepied@gmail.com>
2024-05-14 22:25:31 +0200

    Add filter button

--- FrontEnd/script/script.js
+++ FrontEnd/script/script.js
@@ -1,57 +1,80 @@
+const token = localStorage.getItem("token");
 let works = [];
 let categories = [];
 
-const gallery = document.querySelector(".gallery")
-const filters = document.querySelector('.filters')
+const gallery = document.querySelector(".gallery");
+const filters = document.querySelector(".filters");
+if (token) {
+}
 const getCategories = () => {
   fetch("http://localhost:5678/api/categories")
     .then((response) => {
-      return response.json()
+      return response.json();
     })
     .then((data) => {
       categories = data;
-      displayCategories();
+      if (!token) {
+        displayCategories();
+      }
     });
 };
 const getWorks = () => {
   fetch("http://localhost:5678/api/works")
     .then((response) => {
-      return response.json()
+      return response.json();
     })
     .then((data) => {
       works = data;
-      displayWorks();
+      displayWorks(0);
     });
 };
 
-const displayWorks = () => {
-    for (let i =0 ; i<works.length ; i++) {
-        const figure = document.createElement("figure")
-        const img = document.createElement("img")
-        img.src= works[i].imageUrl
-        img.alt=works[i].title
-        const figcaption = document.createElement("figcaption")
-        figcaption.innerHTML =works[i].title
-        figure.appendChild(img)
-        figure.appendChild(figcaption)
-        gallery.appendChild(figure)
+const displayWorks = (categoryId) => {
+  console.log(categoryId);
+  let fileredWorks = [];
+  if (categoryId === 0) {
+    fileredWorks = works;
+  } else {
+    fileredWorks = works.filter((work) => work.categoryId === categoryId);
+  }
+  const filter_button = document.getElementsByClassName("flt_btn");
+  console.log(filter_button[1]);
+  for (let i = 0; i < filter_button.length; i++) {
+    if (i === categoryId) {
+      filter_button[i].className = "filter_button_active flt_btn";
+    } else {
+      filter_button[i].className = "filter_button flt_btn";
     }
-}
+  }
+  gallery.innerHTML = "";
+  for (let i = 0; i < fileredWorks.length; i++) {
+    const figure = document.createElement("figure");
+    const img = document.createElement("img");
+    img.src = fileredWorks[i].imageUrl;
+    img.alt = fileredWorks[i].title;
+    const figcaption = document.createElement("figcaption");
+    figcaption.innerHTML = fileredWorks[i].title;
+    figure.appendChild(img);
+    figure.appendChild(figcaption);
+    gallery.appendChild(figure);
+  }
+};
 
 const displayCategories = () => {
-    let categoriesList = [...categories]
-    categoriesList.push({
-        "id": 0,
-        "name": "Tous"
-    })
-   categoriesList.sort((a,b) => a.id - b.id); // b - a for reverse sort.sort((a,b) => a.last_nom - b.last_nom); // b - a for reverse sort
-    for (let i =0 ; i<categoriesList.length ; i++) {
-        console.log(categoriesList[i]);
-        const button = document.createElement("button")
-        button.innerHTML = categoriesList[i].name
-        button.className= "filter_button"
-        filters.appendChild(button)
-    }
-}
+  let categoriesList = [...categories];
+  categoriesList.push({
+    id: 0,
+    name: "Tous",
+  });
+  categoriesList.sort((a, b) => a.id - b.id); // b - a for reverse sort.sort((a,b) => a.last_nom - b.last_nom); // b - a for reverse sort
+  for (let i = 0; i < categoriesList.length; i++) {
+    const button = document.createElement("button");
+    button.innerHTML = categoriesList[i].name;
+    button.className = "filter_button flt_btn";
+    button.addEventListener("click", () => displayWorks(categoriesList[i].id));
+    filters.appendChild(button);
+  }
+};
+
 getCategories();
 getWorks();
